c131683ce41dd63bc6bd0d2a44346f310005b3a2
Kashif Siddiqui <kashifshaikh810>
2020-03-28 21:42:39 +0500

    python assignment 1,2,3

---
+++ ssuetass1.py
@@ -0,0 +1,9 @@
+name = "Zain Alam Shaikh"
+f_name = "Hafeez Alam Shaikh"
+residence = "2D 12/11 Nazimabad#2 , Karachi"
+qualification = "Study in University Of Karachi"
+
+print(name)
+print(f_name)
+print(residence)
+print(qualification)
---
+++ ssuetass3.py
@@ -0,0 +1,36 @@
+# Q1
+# numerator = int(input("Enter Numerator: "))
+# denominator = int(input("Enter Denominator: "))
+# if numerator % denominator == 0:
+#     print("Number {} is completely divisible by {}".format(numerator , denominator))
+# else:
+#     print("Number {} is completely not divisible by {}".format(numerator , denominator))
+
+#Q2
+
+# user_input = str(input("Enter String : "))
+# user_want_copy = int(input("How Many Copies of Strings You Need : "))
+# print(user_want_copy ,"Copies of {} are".format(user_input*user_want_copy))
+
+#Q3
+
+# vowels = ["a" , "e" , "i" , "o" , "u" , "A" , "E" , "I" , "O" , "U"]
+# ask_user = str(input("Enter Character : "))
+# if ask_user in vowels:
+#     print("Letter" , ask_user , "is Vowel")
+# else:
+#     print("Letter {} is not Vowel".format(ask_user))
+
+#Q4
+
+# for odd_no in range(1,20,2):
+#     print(odd_no)
+
+#Q5
+
+fav_food = [ "pizza" , "biryani" , "Karahi" , "Chicken Pulao" , "chinese rice" , "shawarma" , "French Fries" , "Daal" , "Katakut" , "Sajji"]
+# print(fav_food)
+print (f"{[0:3]} "are first three foods")
+print (f{fav_food[4:7]} "are middle three foods")
+print (f"{fav_food[7:10]} "are last three foods")
+print (f"{fav_food[7:10]} are last three items .")
